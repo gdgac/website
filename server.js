88618d4f58cbcc21869279f06dbec0679965cfe6
Sebastian Mauer <mauimauer@gmail.com>
2013-12-03 21:49:27 +0100

    Something is fishy...

--- server.js
+++ server.js
@@ -31,6 +31,8 @@ app.configure('development', function(){
 });
 
 app.configure('production', function(){
+  console.log("Production Mode!");
+  console.log(path.join(__dirname, 'public'));
   app.use(express.favicon(path.join(__dirname, 'public', 'favicon.ico')));
   app.use(express.static(path.join(__dirname, 'public')));
   app.set('views', __dirname + '/views');
